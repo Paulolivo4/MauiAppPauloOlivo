679190cf83e73e9bc1eeca8fad8a9e83421d7975
rolan <rolan@DESKTOP-AB5B9CB>
2024-11-29 20:00:21 -0500

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppPauloOlivo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppPauloOlivo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
