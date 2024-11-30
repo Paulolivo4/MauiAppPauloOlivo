39d4d223a02d4c6e550069096797466a65210312
Paulo Olivo <paulo@DESKTOP-AB5B9CB>
2024-11-30 00:19:55 -0500

    Creación y modificación de los archivos locales.

---
+++ POAboutPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace MauiAppPauloOlivo;
+
+public partial class POAboutPage : ContentPage
+{
+	public POAboutPage()
+	{
+		InitializeComponent();
+	}
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+}
